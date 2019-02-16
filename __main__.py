00bcddd062afc77b8897df4bbcb8389c3f069950
Jin Young Bang <jybang@bu.edu>
2019-02-16 18:01:31 -0500

    commit

--- __main__.py
+++ __main__.py
@@ -12,16 +12,6 @@ p = Pyzomato('e2d11e64cba0ac13166aefdeb59df363')
 # google maps sdk and api key for google maps
 gmaps = googlemaps.Client(key = "AIzaSyBx9Cwhpeg0Kg3GMlYQLHkiXnvozWfDD1E")
 
-# class Restaurant:
-#     def __init__(self, i):
-#         self.i = i
-#
-#     def getName(self):
-#         return self.restaurants
-#
-
-# sum of three
-
 
 app = Flask(__name__)
 
@@ -37,59 +27,26 @@ def index1():
     user_text = request.form['location']
     user_price = request.form['price']
 
+    # gets geocode results from user-inputted location
     geocode_result = gmaps.geocode(user_text)
+
+    # latitude and longitude of user-inputted location
     latitude = geocode_result[0]["geometry"]["location"]["lat"]
     longitude = geocode_result[0]["geometry"]["location"]["lng"]
-    restaurants = p.search(lat=latitude, lon=longitude, radius="500")
-    average_cost = (restaurants["restaurants"][0]["restaurant"]["average_cost_for_two"])/2
 
-    # debugging
-    # print(user_text)
-    # print(user_price)
-    # print(average_cost)
+    # searches restaurants based on lat and lon
+    restaurants = p.search(lat=latitude, lon=longitude, radius="500")
+
     dict = {}
 
+    # returns
     for i in range(len(restaurants["restaurants"])):
         average_cost = (restaurants["restaurants"][i]["restaurant"]["average_cost_for_two"])/2
         if average_cost < int(user_price):
             dict[i] = (restaurants["restaurants"][i]["restaurant"]["average_cost_for_two"])/2
 
-    list_of_dict = []
-    list_of_dict_price = []
-    list_of_dict_sorting = []
-    counter = 0
-
-    for i in dict:
-        list_of_dict += [counter, i, (restaurants["restaurants"][i]["restaurant"]["average_cost_for_two"]/2)]
-        list_of_dict_price += [(restaurants["restaurants"][i]["restaurant"]["average_cost_for_two"]/2)]
-        list_of_dict_sorting += [[(restaurants["restaurants"][i]["restaurant"]["average_cost_for_two"]/2), i]]
-        counter += 1
-
-    print(list_of_dict)
-    print()
-    print(list_of_dict_price)
-    print()
-    print(list_of_dict_sorting)
-    print()
-    print(len(list_of_dict_price))
-    print((list_of_dict_sorting)[1][0])
-    print(type(list_of_dict_sorting))
-    print(type(list_of_dict_price))
-    print(type((list_of_dict_sorting)[1][0]))
-
-    numbers = list_of_dict_sorting
-    result = [seq for i in range(len(numbers), 0, -1) for seq in itertools.combinations(numbers[0], i) if sum(seq) <= int(user_price)]
-
-    updated_result = []
-    for i in range(len(result)):
-        if len(result[i]) == 3:
-            updated_result += [result[i]]
-
-    print(updated_result)
-
-
-
     return jsonify(dict)
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
