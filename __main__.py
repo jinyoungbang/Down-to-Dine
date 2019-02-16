8b7a702c44a787eeaf26e6dd482bdcf32ae45f92
Jin Young Bang <jybang@bu.edu>
2019-02-15 23:58:25 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from pyzomato import Pyzomato
 
 p = Pyzomato('e2d11e64cba0ac13166aefdeb59df363')
+gmaps = googlemaps.Client(key = "AIzaSyBx9Cwhpeg0Kg3GMlYQLHkiXnvozWfDD1E")
 
 app = Flask(__name__)
 
@@ -30,6 +31,21 @@ def index1():
 def index2():
     restaurants = p.search(q="Boston")
     return restaurants["restaurants"][0]["restaurant"]["average_cost_for_two"]
+
+@app.route('/googletesting')
+def index3():
+    geocode_result = gmaps.geocode('Boston')
+
+    return jsonify(geocode_result)
+
+@app.route('/googletesting1')
+def index4():
+    geocode_result = gmaps.geocode('Boston')
+    latitude = geocode_result[0]["geometry"]["location"]["lat"]
+    longitude = geocode_result[0]["geometry"]["location"]["lng"]
+    print(latitude)
+    print(longitude)
+    return jsonify(geocode_result[0]["geometry"]["location"])
 
 if __name__ == '__main__':
     app.run()
