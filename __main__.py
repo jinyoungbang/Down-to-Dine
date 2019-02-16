c7c73d2eb99ad4c2d34b9852d50a341a2a52418b
Jin Young Bang <jybang@bu.edu>
2019-02-16 01:11:56 -0500

    changes to main

--- __main__.py
+++ __main__.py
@@ -6,7 +6,9 @@ import googlemaps
 from datetime import datetime
 from pyzomato import Pyzomato
 
+#sdk and api key for zomato
 p = Pyzomato('e2d11e64cba0ac13166aefdeb59df363')
+# google maps sdk and api key for google maps
 gmaps = googlemaps.Client(key = "AIzaSyBx9Cwhpeg0Kg3GMlYQLHkiXnvozWfDD1E")
 
 app = Flask(__name__)
@@ -15,37 +17,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
-
-
-@app.route('/location')
+@app.route('/result', methods=['POST', 'GET'])
 def index1():
-    restaurants = p.search(lat="42.350301", lon="-71.102678", radius="500")
+    text = request.form['text']
+    geocode_result = gmaps.geocode(text)
+    latitude = geocode_result[0]["geometry"]["location"]["lat"]
+    longitude = geocode_result[0]["geometry"]["location"]["lng"]
+    restaurants = p.search(lat=latitude, lon=longitude, radius="500")
     return jsonify(restaurants)
 
 @app.route('/averageprice')
 def index2():
     restaurants = p.search(q="Boston")
     return restaurants["restaurants"][0]["restaurant"]["average_cost_for_two"]
-
-@app.route('/googletesting')
-def index3():
-    geocode_result = gmaps.geocode('Boston')
-
-    return jsonify(geocode_result)
-
-@app.route('/googletesting1')
-def index4():
-    geocode_result = gmaps.geocode('Boston')
-    latitude = geocode_result[0]["geometry"]["location"]["lat"]
-    longitude = geocode_result[0]["geometry"]["location"]["lng"]
-    print(latitude)
-    print(longitude)
-    return jsonify(geocode_result[0]["geometry"]["location"])
 
 if __name__ == '__main__':
     app.run()
