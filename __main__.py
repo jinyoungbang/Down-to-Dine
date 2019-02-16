7f4af7bac7faaf818f6a57f6558f0d1a68440afc
Jin Young Bang <jybang@bu.edu>
2019-02-15 21:42:07 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -11,19 +11,19 @@ p = Pyzomato('e2d11e64cba0ac13166aefdeb59df363')
 app = Flask(__name__)
 
 @app.route('/')
-    def index():
-        return render_template('index.html')
+def index():
+    return render_template('~/Documents/GitHub/HackNYU19/index.html')
 
 @app.route('/', methods=['POST'])
-    def my_form_post():
-        text = request.form['text']
-        processed_text = text.upper()
-        return processed_text
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 
 @app.route('/location')
 def index1():
-    restaurants = p.search(lat="42.350301", lon="-71.102678", radius="", sort=)
+    restaurants = p.search(lat="42.350301", lon="-71.102678", radius="500")
     return jsonify(restaurants)
 
 @app.route('/averageprice')
