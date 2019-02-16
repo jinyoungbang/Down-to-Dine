43f6408161397ec2d0afced4977685175213aac2
Jin Young Bang <jybang@bu.edu>
2019-02-15 21:35:38 -0500

    Pushing py

--- __main__.py
+++ __main__.py
@@ -1,17 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import requests
 import pprint
 import json
 import googlemaps
 from datetime import datetime
+from pyzomato import Pyzomato
 
-gmaps = googlemaps.Client(key='AIzaSyBx9Cwhpeg0Kg3GMlYQLHkiXnvozWfDD1E')
+p = Pyzomato('e2d11e64cba0ac13166aefdeb59df363')
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "testing"
+
+@app.route('/location')
+def index1():
+    restaurants = p.search(lat="42.350301", lon="-71.102678", radius="", sort=)
+    return jsonify(restaurants)
+
+@app.route('/averageprice')
+def index2():
+    restaurants = p.search(q="Boston")
+    return restaurants["restaurants"][0]["restaurant"]["average_cost_for_two"]
 
 if __name__ == '__main__':
     app.run()
